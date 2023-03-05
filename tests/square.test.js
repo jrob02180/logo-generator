fd839ae973abd82c2ba329076bad10ac315ee059
jrob02180 <jrob02180@gmail.com>
2023-03-04 20:13:08 -0700

    added prompts

--- tests/square.test.js
+++ tests/square.test.js
@@ -4,8 +4,9 @@ const square = new Square("square", "blue", "SVG", "white");
 
 describe("square", () => {
     describe("Properties of square", () => {
-        test("Test shape properties")
-        expect(square.shape).toEqual("square")
+        test("Test shape properties", () => {
+            expect(square.shape).toEqual("square")
+        })
     })
     test("Test shapeColor", () => {
         expect(square.shapeColor).toEqual("blue")
---
+++ tests/triangle.test.js
@@ -0,0 +1,20 @@
+const Triangle = require("../lib/triangle");
+
+const triangle = new Triangle("triangle", "blue", "SvG", "pink");
+
+describe("triangle", () => {
+    describe("Properties of triangle", () => {
+        test("Test shape properties", () => {
+            expect(triangle.shape).toEqual("triangle");
+        })
+        test("Test shapeColor", () => {
+            expect(triangle.shapeColor).toEqual("blue");
+        })
+        test("Test text", () => {
+            expect(triangle.text).toEqual("SvG");
+        })
+        test("Test textColor", () => {
+            expect(triangle.textColor).toEqual("pink")
+        })
+    })
+})
